41fb4a7cc8455a2f7eded81be87f044604a6fac9
qiubix <qiubix@gmail.com>
2014-04-18 17:30:03 +0200

    Remove unnecessary GameObject tests

--- test/objects/GameObjectTest.java
+++ test/objects/GameObjectTest.java
@@ -3,15 +3,4 @@ package objects;
 import org.junit.Test;
 
 public class GameObjectTest {
-
-  //TODO: write proper tests for GameObjects, if necessary
-  @Test
-  public void shouldGetPositionXOfAllObjects() throws Exception {
-
-  }
-
-  @Test
-  public void shouldGetPositionYOfAllObjects() throws Exception {
-
-  }
 }
